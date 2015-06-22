f3f5df9a76bc41e97212ca3c75396b347855262b
gaurang9007 <anoopgupta@Anoops-Mac-mini.local>
2015-06-22 07:59:55 +0530

    Initial Commit

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Anoop Gupta on 22/06/15.
+//  Copyright (c) 2015 Ankit Neo GHz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by Anoop Gupta on 22/06/15.
+//  Copyright (c) 2015 Ankit Neo GHz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
