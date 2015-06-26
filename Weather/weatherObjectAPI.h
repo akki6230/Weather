65d30c02dd596e2003da198e3c1a8e291c0990b8
gaurang9007 <anoopgupta@Anoops-Mac-mini.local>
2015-06-26 09:36:58 +0530

    updated

---
+++ Weather/weatherObjectAPI.h
@@ -0,0 +1,13 @@
+//
+//  weatherObjectAPI.h
+//  Weather
+//
+//  Created by Anoop Gupta on 26/06/15.
+//  Copyright (c) 2015 Ankit Neo GHz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface weatherObjectAPI : NSObject
+
+@end
---
+++ Weather/weatherObjectAPI.m
@@ -0,0 +1,14 @@
+//
+//  weatherObjectAPI.m
+//  Weather
+//
+//  Created by Anoop Gupta on 26/06/15.
+//  Copyright (c) 2015 Ankit Neo GHz. All rights reserved.
+//
+
+#define API_KEY @"3ce13b1ab72b89507f4859464e467"
+#import "weatherObjectAPI.h"
+
+@implementation weatherObjectAPI
+
+@end
